1cf6df9adfd7e18fdc2290ce2946c12ce96cf5ca
alef-thallys <alef.thallys22@gmail.com>
2025-05-30 21:03:19 -0300

    Add Docker support with Dockerfile and docker-compose.yml; configure services for app, MariaDB, and RabbitMQ, and update test class name for consistency.

--- src/test/java/com/github/alefthallys/roombooking/RoomBookingApplicationTests.java
+++ src/test/java/com/github/alefthallys/roombooking/RoomBookingApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RoombookingApplicationTests {
+class RoomBookingApplicationTests {
 	
 	@Test
 	void contextLoads() {
